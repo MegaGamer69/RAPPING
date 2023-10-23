8d4cb183b36617ffa90d4b01790b2a1e17e9c3ea
megagamerotario@gmail.com <megagamerotario@gmail.com>
2023-10-23 12:41:14 -0300

    LOL

---
+++ source/Main.hx
@@ -0,0 +1,16 @@
+package;
+
+import flixel.FlxGame;
+
+/**
+ *  @MegaGamerOtario(coder)
+**/
+
+class Main extends FlxGame {
+    public var width:Int = 1280;
+    public var height:Int = 720;
+    
+    public function new():Void {
+        
+    }
+}
