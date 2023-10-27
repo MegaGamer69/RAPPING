b3e9ab67c0cbac4e5b3a4424c88201ec7124e0e6
megagamerotario@gmail.com <megagamerotario@gmail.com>
2023-10-27 19:53:43 -0300

    Cum

---
+++ source/IntroState.hx
@@ -0,0 +1,22 @@
+package;
+
+import flixel.FlxState;
+import flixel.FlxBasic;
+
+/**
+ *  @MegaGamerOtario(coder)
+ *  this game is inspired on Friday Night Funkin'
+**/
+
+class IntroState extends FlxState {
+    // the first state starts here
+    private var skipIntro:Bool = false;
+    
+    override function create():Void {
+        
+    }
+    
+    override function update(elapsed:Float):Void {
+        
+    }
+}
--- source/Main.hx
+++ source/Main.hx
@@ -1,16 +1,55 @@
 package;
 
 import flixel.FlxGame;
+import flixel.FlxState;
+import openfl.Assets;
+import openfl.Lib;
 
 /**
  *  @MegaGamerOtario(coder)
+ *  this game is inspired on Friday Night Funkin'
 **/
 
-class Main extends FlxGame {
-    public var width:Int = 1280;
-    public var height:Int = 720;
+class Main {
+    // a FlxGame settings
+    var width:Int = 1280;
+    var height:Int = 720;
+    var fpsRate:Int = 60;
+    var zoom:Float = -1;
+    var ratioX:Float = 0;
+    var ratioY:Float = 0;
+    var skipSplash:Bool = false;
+    var startFull:Bool = false;
     
-    public function new():Void {
+    var intro:Class<FlxState> = IntroState;
+    
+    public function new() {
+        super();
         
+        // check the intro state if is not null
+        if(intro != null) {
+            init();
+        }
+    }
+    
+    public function main():Void {
+        Lib.current.addChild(new Main());
+    }
+    
+    public static function setup():Void {
+        addChild(new FlxGame(width, height, intro, zoom, fpsRate, skipSplash, startFull));
+        
+        var stageW:Int = Lib.current.stageWidth;
+        var stageH:Int = Lib.current.stageHeight;
+        
+        if zoom = -1 {
+            ratioX = stageW / width;
+            ratioY = stageH / height;
+            
+            zoom = Math.min(ratioX, ratioY);
+            
+            width = Math.ceil(stageW / zoom);
+            height = Math.ceil(stageH / zoom);
+        }
     }
 }
